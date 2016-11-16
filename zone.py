226e5449f927d48e12bcfd284bd369918f8f0072
danwms3 <danwms@yahoo.com>
2016-11-15 20:14:25 -0600

    Working zone.py with fixed variables for inputs

--- zone.py
+++ zone.py
@@ -1,124 +1,66 @@
-"""
- NX-API-BOT 
-"""
 import requests
 import json
 
-"""
-Modify these please
-"""
-url='http://172.22.163.35:8080/ins'
-switchuser='danwms'
-switchpassword='AICteam'
-
-# Pwwns will be passed in
-OldPwwn = '11:11:11:11:11:11:11:11'
+#print "enter ip address"
+#ip=raw_input()
+ip="172.22.163.35:8080"
+OldPwwn = u'11:11:11:11:11:11:11:11'
+OldDevAlias = 'OldDeviceAlias'
 NewPwwn = '33:33:33:33:33:33:33:33'
-# the following will be derived from show zoneset active once I figure out how to parse the return string
-zone = 'IMzone1'
-zoneset = 'IMzoneset'
-vsan = 10
 
-debug=0
+#print "enter vsan to be configured"
+#vlanId=raw_input()
+#vsanId=raw_input()
 
-#
-# first, get existing zoneset
-#
-myheaders={'content-type':'application/json-rpc'}
+myheaders = {'content-type': 'application/json-rpc'}
+url = "http://"+ip+"/ins"
+username = "danwms"
+password = "AICteam"
+
+
 payload=[
-  {
-    "jsonrpc": "2.0",
-    "method": "cli",
-    "params": {
-      "cmd": "show zoneset active",
-      "version": 1.2
-    },
-    "id": 1
-  }
+  {"jsonrpc": "2.0","method": "cli","params": {"cmd": "show zoneset active","version": 1},"id": 1},
 ]
-response = requests.post(url,data=json.dumps(payload), headers=myheaders,auth=(switchuser,switchpassword)).json()
+response = requests.post(url,data=json.dumps(payload), headers=myheaders,auth=(username,password)).json()
 
-#
-# DW: add logic to check if OldPwwn is in exising active zoneset, extract Zoneset name, Zone name and VSAN number
-# DW: also determine if enhanced zoning is being used - if so, a commit is required before activating zoneset
-#
-# now add new member to existing zoneset
-#
+vsan_table = response['result']['body']['TABLE_zoneset']['ROW_zoneset']
+v1=vsan_table[0]
 
-myheaders={'content-type':'application/json-rpc'}
+rezone={}
+for zoneset_vsan in vsan_table:
+    for zones in zoneset_vsan['TABLE_zone']['ROW_zone']:
+        if isinstance(zones,dict):
+            try:
+                for zone_member in zones['TABLE_zone_member']['ROW_zone_member']:
+                    try:
+                        if zone_member.get('wwn') is not None and zone_member.get('wwn') is OldPwwn:
+                            pwwn = zone_member.get('wwn')
+                            zonename = zones['zone_name']
+                            vsanID = zones['zone_vsan_id']
+                            zoneset = zoneset_vsan['zoneset_name']
+                    except:
+                        print ('top loop debug')
+            except:
+                print ('debug: do not need this print')
+        else:
+            try:
+                zones = zoneset_vsan['TABLE_zone']['ROW_zone']
+                for zone_member in zones['TABLE_zone_member']['ROW_zone_member']:
+                    if zone_member.get('wwn') is not None and zone_member.get('wwn') == OldPwwn:
+                        pwwn = zone_member.get('wwn')
+                        zonename = zones['zone_name']
+                        vsanID = zones['zone_vsan_id']
+                        zoneset = zoneset_vsan['zoneset_name']
+            except:
+                print "debug: failed zones assignmend, bottom loop"
+
+
+# this creates vsan 5
 payload=[
-  {
-    "jsonrpc": "2.0",
-    "method": "cli",
-    "params": {
-      "cmd": "zone name IMzone1 vsan 10",
-      "version": 1.2
-    },
-    "id": 1
-  },
-  {
-    "jsonrpc": "2.0",
-    "method": "cli",
-    "params": {
-      "cmd": "member pwwn 33:33:33:33:33:33:33:33",
-      "version": 1.2
-    },
-    "id": 2
-  },
+ {"jsonrpc": "2.0","method": "cli","params": {"cmd": "conf t","version": 1},"id": 1},
+ {"jsonrpc": "2.0","method": "cli","params": {"cmd": "zone name "+zonename+" vsan "+vsanID,"version": 1},"id": 2},
+ {"jsonrpc": "2.0","method": "cli","params": {"cmd": "member pwwn "+NewPwwn,"version": 1},"id": 3},
+ {"jsonrpc": "2.0","method": "cli","params": {"cmd": "zoneset activate name "+zoneset+" vsan "+vsanID,"version": 1},"id": 4}
 ]
-response = requests.post(url,data=json.dumps(payload), headers=myheaders,auth=(switchuser,switchpassword)).json()
-
-#
-# if enhanced zoning, commit changes
-#
-# first, gett he zoning method status
-#
-myheaders={'content-type':'application/json-rpc'}
-payload=[
-  {
-    "jsonrpc": "2.0",
-    "method": "cli",
-    "params": {
-      "cmd": "show zone status vsan 10",
-      "version": 1.2
-    },
-    "id": 1
-  }
-]
-response = requests.post(url,data=json.dumps(payload), headers=myheaders,auth=(switchuser,switchpassword)).json()
-print (json.dumps(response, indent=4))
-
-# DW: then test whether string "mode: enhanced" was returned
-
-myheaders={'content-type':'application/json-rpc'}
-payload=[
-  {
-    "jsonrpc": "2.0",
-    "method": "cli",
-    "params": {
-      "cmd": "zone commit vsan 10",
-      "version": 1.2
-    },
-    "id": 1
-  }
-]
-response = requests.post(url,data=json.dumps(payload), headers=myheaders,auth=(switchuser,switchpassword)).json()
-
-#
-# and activate zoneset
-#
-myheaders={'content-type':'application/json-rpc'}
-payload=[
-  {
-    "jsonrpc": "2.0",
-    "method": "cli",
-    "params": {
-      "cmd": "zoneset activate name IMzoneset vsan 10",
-      "version": 1.2
-    },
-    "id": 1
-  }
-]
-response = requests.post(url,data=json.dumps(payload), headers=myheaders,auth=(switchuser,switchpassword)).json()
-
-#print (json.dumps(response, indent=4))
+response = requests.post(url,data=json.dumps(payload), headers=myheaders,auth=(username,password)).json()
+print (json.dumps(response[3]['result']['msg'], indent=4))
