766478fc0719266c0cd7c8e2bd7710877d148e96
micbonne <micbonne@cisco.com>
2016-11-17 14:26:27 -0600

    Adding intial documentation to the aic_app.py file

--- aic_api.py
+++ aic_api.py
@@ -10,6 +10,9 @@ import sys
 
 app = Flask(__name__)
 
+# The dictionary below, named "api_methods", lists the various URL paths that are
+# available in the Flask API.
+
 api_methods = {
     '/' : 'Your current location.',
     '/status'  : 'Displays the status of the API.',
@@ -17,10 +20,20 @@ api_methods = {
     '/task' : 'Task operations. '
 }
 
+# The dictionrary below, named "registred_plugins", may be used to list
+# all of the available plugins within the AIC Framework. In the future, we should
+# consider developing a framework for registering additional plugins.
+# The "nxapi" plugin is used primalry to test the NX-API.
+# In the intial release of the AIC Framework, only the "hba_swap" plugin will be used.
+
 registered_plugins = {
     'hba_swap' : 'HBA Swap is used to...',
     'nxapi'   : 'Shim for NX-API tetsing.'
 }
+
+# The dictionary below, named "credentials", could be used for storing the credentials
+# the App will need to access the MDS and other network devices.
+# As of 11-17-16, the credentials below aren't used by Dan's "zone.py" code.
 
 credentials = [
     {
@@ -30,8 +43,21 @@ credentials = [
     }
 ]
 
+# The array below, named "tasks", is used to store information related to the tasks
+# that have been submitted to the API.
+#
+# The array is initilized below with no data.  The array will contain dictonaries,
+# each of which will contain at a minimum: ip_address & selected-plugin. It will 
+# likley also contain the status of the task. 
+
 tasks = [
 ]
+
+# The fucntion below is used to route the submitted task to the correct plugin.
+# The function takes two paramters: "plugin" and "ip_address".
+#
+# As of 11-16-16 the "plugin" parameter must be either: "hba_swap" or "nxapi".
+# For now, the code includes DEBUG output that is written to stdout. 
 
 def call_selected_plugin(plugin, ip_address):
     sys.stdout.write('DEBUG: call_selected_plugin(): Plugin requested is "'+plugin+'".\n')
@@ -42,6 +68,17 @@ def call_selected_plugin(plugin, ip_address):
         sys.stdout.write('DEBUG: call_selected_plugin(): Calling call_nxaip().\n')
         call_nxapi(ip_address)
     return 
+
+# The function below named "hba_swap" is used to invoke thet HBA SWAP workflow. 
+# This function will likley be removed or refactored to call Dan's code in 
+# the "zone.py" file.
+#
+# This function includes DEBUG output that is written to stdout.
+# The "payload" dictionray defined in the fucntion includes the parameters required 
+# by the NX-API.
+# 
+# The fucntion also calls the "call_nxapi" fucntion with the "ipaddress" and "payload" 
+# variables.
 
 def hba_swap(ipaddress):
     sys.stdout.write('DEBUG: hba_swap(): Executing "hba_swap" plugin.\n')
@@ -60,6 +97,19 @@ def hba_swap(ipaddress):
     sys.stdout.write('DEBUG: hba_swap(): Calling call_nxapi().\n')
     call_nxapi(ipaddress, payload)
     return
+
+# The function below, named "call_nxapi", is used to access the NX-API. 
+# This function will likley refactored to call Dan's code in 
+# the "zone.py" file.
+#
+# This function includes DEBUG output that is written to stdout.
+# 
+# For now, the MDS credeintals are defined in the variables: "switchuser" & 
+# "switchpassword".
+#
+# The URL variable is used to build the base NX-API URL.
+# This function returns the JSON response recived from the NX-API.
+
 
 def call_nxapi(ip_address, payload):
     sys.stdout.write('DEBUG: call_nxapi: Executing "nxapi" plugin\n')
