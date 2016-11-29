a7b8ac1219b5e993b2859da4e412ce00249ed201
michaelbonnettjr <micbonne@cisco.com>
2016-11-29 10:49:35 -0600

    Incorporating Dan's latest changes to the zone_bonnett.py file.

--- zone_bonnett.py
+++ zone_bonnett.py
@@ -1,37 +1,49 @@
-from flask import jsonify
 import requests
 import json
 import sys
-import pprint
 
+# Note for testing - you can remove the 33:33:33:33:33:33:33:33 zone member from the MDS switch using
 #
-# input ( ip, Old Pwwn, New Pwwn ) to program. Using fixed values here for testing
-# DW - add device alias option if time permits
-#
-# note for testing - you can remove the 33:33:33:33:33:33:33:33 zone member from the MDS switch using
-#
-#               config t
-#               zone name IMzone1 vsan 10
-#               no member pwwn 33:33:33:33:33:33:33:33
-#               zoneset activate name IMzoneset vsan 10
-#
+#    config t
+#    zone name IMzone1 vsan 10
+#    no member pwwn 33:33:33:33:33:33:33:33
+#    zoneset activate name IMzoneset vsan 10
 
-
-#
-# credentials for switch - need to encrypt these
-#
+# Credentials for switch - need to encrypt these
 username = "danwms"
 password = "AICteam"
 
+# The update_zone() function below, adds tthe NewPwwn to the zone where we found the OldPwwn.
+
+def update_zone(ipaddress, zname, vID, NPwwn, zset):
+    sys.stdout.write('DEBUG: update_zone(): Executing "update_zone" function.\n')
+
+    header = {'content-type': 'application/json-rpc'}
+    url = "http://" + ipaddress + ":8080/ins"
+
+    payload = [
+        {"jsonrpc": "2.0", "method": "cli", "params": {"cmd": "conf t", "version": 1}, "id": 1},
+        {"jsonrpc": "2.0", "method": "cli", "params": {"cmd": "zone name " + zname + " vsan " + vID, "version": 1}, "id": 2},
+        {"jsonrpc": "2.0", "method": "cli", "params": {"cmd": "member pwwn " + NPwwn, "version": 1}, "id": 3},
+        {"jsonrpc": "2.0", "method": "cli", "params": {"cmd": "zoneset activate name " + zset + " vsan " + vID, "version": 1}, "id": 4}
+    ]
+
+    response = requests.post(url, data=json.dumps(payload), headers=header, auth=(username, password)).json()
+    print "DEBUG: update_zone(): zone",zname,"added member",NPwwn,"with message:",json.dumps(response[3]['result']['msg'], indent=4)
+    print 'DEBUG: update_zone(): NX-APIs Response 2:'
+    print (json.dumps(response[2]['result']['msg'], indent=4))
+    print 'DEBUG: update_zone(): NX-APIs Response 3:'
+    print (json.dumps(response[3]['result']['msg'], indent=4))
+
+# configure_zone() takes the switch ip address, OldPwwn, Old Device Alias (future),
+# and the New Pwwn.
 #
-# configure_zone() takes the switch ip address/url, the header for the request, OldPwwn, Old Device Alias (future),
-#  and the New Pwwn to add to the zone.
 # Routine first searches existing the zoneset for instances of the Old Pwwn
 # and then adds the NewPwwn to those zones and activates the new updated zoneset.
 # As time permits, Device Alias capability will also be added
-#
 
 def configure_zone(ipaddress, OldPwwn, OldDevAlias, NewPwwn):
+    # Write DEBUG output to stdout.
     sys.stdout.write('DEBUG: configure_zone(): Executing "configure_zone" function.\n')
     sys.stdout.write('DEBUG: configure_zone(): IP address is: '+ipaddress+'\n')
     sys.stdout.write('DEBUG: configure_zone(): OldPwwn is: '+OldPwwn+'\n')
@@ -41,81 +53,49 @@ def configure_zone(ipaddress, OldPwwn, OldDevAlias, NewPwwn):
     header = {'content-type': 'application/json-rpc'}
     url = "http://" + ipaddress + ":8080/ins"
 
-    #
-    # get current zoneset info from switch
-    #
+    # Get current zoneset info from switch.
     payload=[
       {"jsonrpc": "2.0","method": "cli","params": {"cmd": "show zoneset active","version": 1},"id": 1},
     ]
+
     response = requests.post(url,data=json.dumps(payload), headers=header,auth=(username,password)).json()
     zone_table = response['result']['body']['TABLE_zoneset']['ROW_zoneset']
+    # Print DEBUG output.
     print 'DEBUG: configure_zone(): Output of "show zoneset active" command:'
     print json.dumps(response, sort_keys=True, indent=4, separators=(',', ': '))
     print 'DEBUG: configure_zone(): Output of "zone_table" variable:'
     print json.dumps(zone_table, sort_keys=True, indent=4, separators=(',', ': '))
 
-    #
     # check current zoneset for the old pwwn. If found, add the new pwwn to all zones that contain the old one
     # and activate the zoneset
     #
     # start by parsing the zoneset table
-    #
     for zoneset_vsan in zone_table:
-        for zones in zoneset_vsan['TABLE_zone']['ROW_zone']:
-    #	Ran into a problem parsing the json structure.
-    #   If only a single zone in present in the zoneset, ROW_zone is type dict rather than list. So I need to
-    #   testing type and adjust 'zone' accordingly (kludgy)
-    #
-            if isinstance(zones,dict):
+        for zones in zoneset_vsan['TABLE_zone']['ROW_zone']:#
+         # Parsing inconsistant json structure. If single zone in zoneset, 
+         # ROW_zone is type dict rather than list. 
+         # Testing type and proceeding accordingly.
+            if isinstance(zoneset_vsan['TABLE_zone']['ROW_zone'],dict):
+                # single zone in zoneset, type dict rather than list so assign directly to avoid error
+                zones = zoneset_vsan['TABLE_zone']['ROW_zone']
                 print 'DEBUG: configure_zone(): ROW_zone: appears to be a "dict":'
                 print json.dumps(zones, sort_keys=True, indent=4, separators=(',', ': '))
+                # more than one zone, cycle through them
+            for zone_member in zones['TABLE_zone_member']['ROW_zone_member']:
+                print 'DEBUG: configure_zone(): zone_member:'
+                print json.dumps(zone_member, sort_keys=True, indent=4, separators=(',', ': '))
                 try:
-                    for zone_member in zones['TABLE_zone_member']['ROW_zone_member']:
-                        print 'DEBUG: configure_zone(): zone_member:'
-                        print json.dumps(zone_member, sort_keys=True, indent=4, separators=(',', ': '))
-                        try:
-                            print 'DEBUG: configure_zone(): The pwnn is: '+zone_member.get('wwn')
-                            if zone_member.get('wwn') is not None and zone_member.get('wwn') == OldPwwn:
-                                print 'DEBUG: configure_zone(): We have a match! Setting variables needed for the NX-API call to the MDS.'
-                                pwwn = zone_member.get('wwn')
-                                zonename = zones['zone_name']
-                                vsanID = zones['zone_vsan_id']
-                                zoneset = zoneset_vsan['zoneset_name']
-    # DW- should make a call here to add pwwn to zone here to account for case where pwwn exists in multiple zones
-                        except:
-                            print ('DEBUG: configure_zone(): top loop debug')
+                    if zone_member.get('wwn') is not None and zone_member.get('wwn') == OldPwwn:
+                        print 'DEBUG: configure_zone(): We have a match! Setting variables needed for the NX-API call to the MDS.'
+                        pwwn = zone_member.get('wwn')
+                        zonename = zones['zone_name']
+                        vsanID = zones['zone_vsan_id']
+                        zoneset = zoneset_vsan['zoneset_name']
+                        print 'DEBUG: configure_zone(): Calling and passing variables to update_zone().'
+                        # Call update_zone to add new pwwn to zone and activate
+                        update_zone(ipaddress, zonename, vsanID, NewPwwn, zoneset)
                 except:
-                    print ('DEBUG: configure_zone(): do not need this print')
-            else:
-                print 'DEBUG: configure_zone(): ROW_zone appears to be a "list".'
-                try:
-                    zones = zoneset_vsan['TABLE_zone']['ROW_zone']
-                    for zone_member in zones['TABLE_zone_member']['ROW_zone_member']:
-                        if zone_member.get('wwn') is not None and zone_member.get('wwn') == OldPwwn:
-                            pwwn = zone_member.get('wwn')
-                            zonename = zones['zone_name']
-                            vsanID = zones['zone_vsan_id']
-                            zoneset = zoneset_vsan['zoneset_name']
-    # DW- should make a call here to add pwwn to zone here to account for case where pwwn exists in multiple zones
-                except:
-                    print "DEBUG: configure_zone(): failed zones assignmend, bottom loop"
-
-    # Here we add the NewPwwn to the zone where we found the OldPwwn
-    #
-    # DW- make this subroutine called when match is found
-    payload=[
-     {"jsonrpc": "2.0","method": "cli","params": {"cmd": "conf t","version": 1},"id": 1},
-     {"jsonrpc": "2.0","method": "cli","params": {"cmd": "zone name "+zonename+" vsan "+vsanID,"version": 1},"id": 2},
-     {"jsonrpc": "2.0","method": "cli","params": {"cmd": "member pwwn "+NewPwwn,"version": 1},"id": 3},
-     {"jsonrpc": "2.0","method": "cli","params": {"cmd": "zoneset activate name "+zoneset+" vsan "+vsanID,"version": 1},"id": 4}
-    ]
-    print 'DEBUG: configure_zone(): Making NX-APIs calls to MDS...'
-    response = requests.post(url,data=json.dumps(payload), headers=header,auth=(username,password)).json()
-    print 'DEBUG: configure_zone(): NX-APIs Response 2:'
-    print (json.dumps(response[2]['result']['msg'], indent=4))
-    print 'DEBUG: configure_zone(): NX-APIs Response 3:'
-    print (json.dumps(response[3]['result']['msg'], indent=4))
-
+                    print ('top loop debug')
 
 def zone():
 
